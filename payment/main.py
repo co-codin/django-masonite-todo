0a9a2b83642f1eb94b5f3d70ca88bed59cd70cbf
elijah <cuiyeqing960904@gmail.com>
2023-04-10 15:57:43 -0400

    payment api added

--- payment/main.py
+++ payment/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
 from fastapi.background import BackgroundTasks
+import requests, time
 
 app = FastAPI()
 
@@ -13,9 +14,10 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+# another redis
 redis = get_redis_connection(
     host="localhost",
-    port=6379,
+    port=6378,
     decode_responses=True
 )
 
@@ -40,3 +42,27 @@ def get(pk: str):
 async def create(request: Request, background_tasks: BackgroundTasks):
     body = await request.json()
 
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+
+    order.save()
+
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd('order_completed', order.dict(), '*')
