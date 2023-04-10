21dc00dd5efbf294e03de542b46b01c78583b4f3
elijah <cuiyeqing960904@gmail.com>
2023-04-10 11:19:01 -0400

    cors added

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'status': 'ok'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+@app.get('/products')
+def all():
+    return Product.all_pks()
